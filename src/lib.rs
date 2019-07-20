2cd2d042fd2e80a3c03bedbcc40a885cccd1cd5c
Filipp Kramer <krame490@gmail.com>
2019-07-20 14:13:11 -0700

    add resizing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::mem;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
@@ -5,12 +6,14 @@ const INITIAL_NBUCKETS: usize = 1;
     
 pub struct HashMap<K, V> {
     buckets: Vec<Vec<(K, V)>>,
+    items: usize,
 }
 
 impl<K, V> HashMap<K, V> {
     pub fn new() -> Self {
         HashMap {
             buckets: Vec::new(), 
+            items: 0,
         }
     }
 }
@@ -20,14 +23,19 @@ where
     K: Hash + Eq,
 {
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        if self.buckets.is_empty() || self.items > 3 * self.buckets.len() / 4 {
+            self.resize();
+        }
+
+
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
         let bucket = (hasher.finish() % self.buckets.len() as u64) as usize;
         let bucket = &mut self.buckets[bucket];
-
+    
+        self.items += 1;
         for &mut (ref ekey, ref mut evalue) in bucket.iter_mut() {
             if ekey == &key {
-                use std::mem;
                 return Some(mem::replace(evalue, value));
             }
         }
@@ -41,6 +49,22 @@ where
             0 => INITIAL_NBUCKETS,
             n => 2 * n,
         };
+
+        let mut new_buckets = Vec::with_capacity(target_size);
+        new_buckets.extend((0..target_size).map(|_| Vec::new())); 
+
+        for (key, value) in self
+            .buckets
+            .iter_mut()
+            .flat_map(|bucket| bucket.drain(..)) 
+        {
+            let mut hasher = DefaultHasher::new();
+            key.hash(&mut hasher);
+            let bucket = (hasher.finish() % new_buckets.len() as u64) as usize;
+            new_buckets[bucket].push((key, value));
+        }
+
+        mem::replace(&mut self.buckets, new_buckets);
     }
 }
 
